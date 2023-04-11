5d2ba5ec63c92439a92d9ee4414eb5131fc81d73
shwetha-97 <42267522+shwetha-97@users.noreply.github.com>
2023-04-11 00:57:55 -0400

    Update apigClient after redeploying cloud formation

--- apiGateway-js-sdk/apigClient.js
+++ apiGateway-js-sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://q0uw4snir1.execute-api.us-east-1.amazonaws.com/v1';
+    var invokeUrl = 'https://lblhl26988.execute-api.us-east-1.amazonaws.com/v1';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
